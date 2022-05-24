c9300b82daabe3cd17a113db990511a7b24560ea
“Pascwin” <pascal-winner@web.de>
2022-05-24 22:23:44 +0200

    styled the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,28 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+// import "./header.module.scss"
+import * as headerStyles from "./header.module.scss"
+
 const Header = () => {
   return (
-    <header>
+    <header className={headerStyles.header}>
       <h1>
-        Palle Fullstack
+        <Link className={headerStyles.title} to="/" >Pascal Winner</Link>
       </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,16 +3,20 @@ import PropTypes from "prop-types"
 
 import Header from "./header"
 import Footer from "./footer"
-import "./layout.css"
+import "../styles/index.scss"
+import * as layoutStyles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
 
   return (
     <>
-      <Header />
-        {children}
-      <Footer />
-
+      <div className={layoutStyles.container}>
+        <div className={layoutStyles.content}>
+          <Header />
+          {children}
+        </div>
+        <Footer />
+      </div>
     </>
   )
 }
